5697f08ecf6cd8c6f012d3e78a73180de078c5ca
mariano <marianoksairi@gmail.com>
2020-07-29 12:06:50 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8165ab3f355de7d52060fcaa08266425.js"
+  "/precache-manifest.1918828c287e17285ea7c8f90e39bc61.js"
 );
 
 workbox.skipWaiting();
